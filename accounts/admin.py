b9a71d902e1e418494471f2dd4b33c8dc5ce9aac
Hellen Wainaina <hellenwain@gmail.com>
2023-01-04 16:37:42 +0300

    change on the user model and Provider Profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import *
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+@admin.register(ServiceProviderProfile)
+class ProviderProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'location', 'phone_number']
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,27 @@
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    bio = models.TextField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class ServiceProviderProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.user.first_name
+
+    @admin.display(ordering='user__username')
+    def username(self):
+        return self.user.username
