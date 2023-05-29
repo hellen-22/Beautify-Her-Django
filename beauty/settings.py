ebf59bddb57acdfa729f4d4c81496f0fcbf3bd1b
Hellen Wainaina <hellenwain@gmail.com>
2023-05-29 12:41:17 +0300

    Adding djoser

--- beauty/settings.py
+++ beauty/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_yasg",
     "rest_framework_simplejwt",
+    "djoser",
 
     "accounts",
     "services",
--- beauty/urls.py
+++ beauty/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
 
     path('', include('accounts.urls')),
     path('services/', include('services.urls')),
+    # path('auth/', include("djoser.urls")),
 
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
